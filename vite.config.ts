4f93e3274a68da29af3fe8fb9fe2e3016f818635
Ankit <nishantkumarsingh0852@gmail.com>
2026-05-14 12:44:21 +0530

    new one

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
-    tanstackStart({}),
+    tanstackStart(),
     tsconfigPaths(),
     react(),
     tailwindcss(),
