1bb85785ca8eda7d2ea9fbcc6d038dc32ca15ca8
Ankit <nishantkumarsingh0852@gmail.com>
2026-05-14 11:26:54 +0530

    new onw

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { tanstackStart } from "@tanstack/react-start/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
-    tsconfigPaths(),
-    react(),
-    tailwindcss(),
     tanstackStart({
       server: { entry: "src/server.ts" },
     }),
+    tsconfigPaths(),
+    react(),
+    tailwindcss(),
   ],
 });
